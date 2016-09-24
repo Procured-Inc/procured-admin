ae6a0b91ce8d65f7f636f59df484dd9ce3995ca3
hzaun <r.nuzha@gmail.com>
2016-09-24 18:41:31 +0530

    Table flip-sorts

--- js/results.js
+++ js/results.js
@@ -5,6 +5,11 @@ var data = [
 		"score":"34"
 	},
 	{
+		"name":"Vikash",
+		"sid":"911",
+		"score":"73"
+	},
+	{
 		"name":"Prajwal",
 		"sid":"423",
 		"score":"54"
@@ -13,6 +18,16 @@ var data = [
 		"name":"Shubham",
 		"sid":"523",
 		"score":"64"
+	},
+	{
+		"name":"Jyoti",
+		"sid":"023",
+		"score":"74"
+	},
+	{
+		"name":"Debapriya",
+		"sid":"451",
+		"score":"46"
 	}
 ];
 
@@ -22,37 +37,85 @@ var data = [
 // 			+"<td>STUD_ID_SCORE</td>"
 // 			+"</tr>";
 
-		for(var ix in data){
-			setTimeout(fill(ix,data), 2000);
-		}
+// function makeTable(){
+	for(var ix in data){
+		var trow = "<tr>"
+						+"<td>"+data[ix].sid+"</td>"
+						+"<td>"+data[ix].name+"</td>"
+						+"<td>"+data[ix].score+"</td>"
+						+"</tr>";
+				$('#t_body').append(trow);
+	}
+// }
+// makeTable();
 
-function fill(ix,data){	
-			var trow = "<tr>"
-					+"<td>"+data[ix].sid+"</td>"
-					+"<td>"+data[ix].name+"</td>"
-					+"<td>"+data[ix].score+"</td>"
-					+"</tr>";
-			$('#t_body').append(trow);
+
+// sort by score
+var switcher = Boolean(0);
+
+function compD(b,a){
+		return a.score-b.score;
 }
-
+function compA(a,b){
+		return a.score-b.score;
+}
 
 function sortByScore(){
-	var r_kids = document.getElementById('t_body').children;
-	for(var jx=0;jx<r_kids.length-1;jx++){
-		for(var kx=1;kx<r_kids.length;kx++){
-			var val1 = r_kids[jx].children[2].innerText;
-			var val2 = r_kids[kx].children[2].innerText;
-
-			if(val1<val2){
-
-				for(var lx in r_kids ){
-					var temp=r_kids[jx].children[lx].innerText;
-					r_kids[jx].children[lx].innerText=r_kids[kx].children[lx].innerText;
-					r_kids[kx].children[lx].innerText=temp;
-				}
-				
-			}
-		}
+	switcher=(!switcher);
+	if(switcher){
+		data.sort(compD);
+	}else{
+		data.sort(compA);
 	}
-
+	// data.sort(comp);
+	$('#mytable tbody').empty();
+	for(var ix in data){
+		var trow = "<tr>"
+						+"<td>"+data[ix].sid+"</td>"
+						+"<td>"+data[ix].name+"</td>"
+						+"<td>"+data[ix].score+"</td>"
+						+"</tr>";
+				$('#t_body').append(trow);
+	}
 }
+// function sortByScore(){
+//        var rkl = document.getElementById('t_body').children.length;
+//        for(var jx=0;jx<rkl-1;jx++){
+//                for(var kx=1;kx<rkl;kx++){
+//        				var r_kids = document.getElementById('t_body').children;
+//                        var val1 = r_kids[jx].children[2].innerText;
+//                        var val2 = r_kids[kx].children[2].innerText;
+
+//                        if(val1<val2){
+//                        	// console.log(r_kids.children);
+
+//                                for(var lx in r_kids.children ){
+//                                        var temp=document.getElementById('t_body').children[jx].children[lx].innerText;
+//                                        document.getElementById('t_body').children[jx].children[lx].innerText=r_kids[kx].children[lx].innerText;
+//                                        document.getElementById('t_body').children[kx].children[lx].innerText=temp;
+//                                }
+
+//                        }
+
+//                        // console.log(r_kids.children);
+//                        document.getElementById('t_body').children=r_kids
+//                    }
+//                }
+//            }
+
+// function sorter(){
+//        var kids = document.getElementById('t_body').children;
+
+//        kids.sort(function(a,b){
+//        				   var val1 = kids[a].children[2].innerText;
+//                        var val2 = kids[b].children[2].innerText;
+//                        return val1-val2;
+//        });
+ 
+//  		$.each(kids, function(index, row) {
+//    			 $('#t_body').children('tbody').append(row);
+//   		});
+
+// }
+
+// $("#myTable").tablesorter();
